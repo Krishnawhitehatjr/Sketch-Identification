896a68c0adbac7126b4167b3fb2bbd047d42b351
Krishnawhitehatjr <99812564+Krishnawhitehatjr@users.noreply.github.com>
2022-11-01 19:51:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function preload()
+{
+    classifier = ml5.imageClassifier('Doodlenet')
+}
+function setup(){
+    canvas=createCanvas(375,375)
+    canvas.center()
+    background("white")
+    canvas.mouseReleased(classifyCanvas);
+    synth= window.speechSynthesis
+}
+function draw()
+{
+    strokeWeight(5)
+    stroke(0)
+
+    if (mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY)
+    }
+}
+function classifyCanvas()
+{
+    classifier.classify(canvas,gotResult);
+}
+function clearCanvas(){
+    background("white")
+}
+function gotResult(error, results)
+{
+    if (error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
